c7dcdcd5ef1171431df463dfcc10ff939bf39f94
yassineprojs <116946536+yassineprojs@users.noreply.github.com>
2024-05-12 09:46:02 +0100

    Delete passwordHash.js

--- passwordHash.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-import dotenv from "dotenv";
-
-dotenv.config();
-const saltRounds = 10;
-
-const hashedPassword = bcrypt.hashSync(process.env.realPassword, saltRounds);
-export default hashedPassword;
